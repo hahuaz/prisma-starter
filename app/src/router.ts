7ca60ba5d8d162b61569d27e0e37e2317d37a5fa
Hasan <dev.hahuaz@gmail.com>
2024-06-11 10:38:31 +0300

    feat: add unused var eslint rule

--- app/src/router.ts
+++ app/src/router.ts
@@ -5,14 +5,14 @@ import {
   personRouter,
   serviceRouter,
   skillRouter,
-} from "./routes";
+} from "@/routes";
 
 /**
  * Setup the routes for the express app
  */
 const router = (app: Express) => {
   return app
-    .get("/", async (req: Request, res: Response) => {
+    .get("/", async (_req: Request, res: Response) => {
       res.json({ message: "App is up and running" });
     })
     .use("/services", serviceRouter)
