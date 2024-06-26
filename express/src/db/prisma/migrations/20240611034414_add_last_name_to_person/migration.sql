0f75097733a92b5e8b4080b350d33d9efa32e843
Hasan <dev.hahuaz@gmail.com>
2024-06-26 20:34:56 +0300

    chore: remove unused dbs

--- express/src/db/prisma/migrations/20240611034414_add_last_name_to_person/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `lastName` to the `Person` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Person" ADD COLUMN     "lastName" TEXT NOT NULL;
