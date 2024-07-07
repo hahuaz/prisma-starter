a229786d8e160cbce694ad6079bee2802c0d4821
Hasan <dev.hahuaz@gmail.com>
2024-07-07 12:00:00 +0300

    fix: rabbit mq is not ready for connection

--- express/src/config/rabbitmq.ts
+++ express/src/config/rabbitmq.ts
@@ -6,23 +6,32 @@ import { sleep } from "@/lib";
 let channel: Channel;
 
 export async function getRabbitMQChannel(retries = 5, delay = 15000) {
-  if (!channel) {
-    let attempts = 0;
-    while (attempts < retries) {
-      try {
-        console.log("Attempt to connect RabbitMQ...");
-        const connection = await amqplib.connect("amqp://rabbitmq:5672");
-        channel = await connection.createChannel();
-        console.log("Connected to RabbitMQ");
-        break; // Exit the loop if connection is successful
-      } catch (error) {
-        console.error("Failed to connect to RabbitMQ", error);
-        attempts++;
-        await sleep(delay);
+  if (channel) {
+    return channel;
+  }
+
+  let attempts = 0;
+  while (attempts < retries) {
+    try {
+      console.log("Attempt to connect RabbitMQ...");
+      const connection = await amqplib.connect("amqp://rabbitmq:5672");
+      channel = await connection.createChannel();
+      console.log("Connected to RabbitMQ");
+      break; // Exit the loop if connection is successful
+    } catch (error) {
+      console.error(
+        `Failed to connect to RabbitMQ. Retrying in ${delay / 1000} seconds.`,
+        error
+      );
+      if (attempts === retries - 1) {
+        console.error(
+          "Failed to connect to RabbitMQ after several attempts. Gracefully shutting down."
+        );
+        process.exit(1);
       }
-    }
-    if (!channel) {
-      throw new Error("Unable to connect to RabbitMQ after several attempts");
+
+      attempts++;
+      await sleep(delay);
     }
   }
   return channel;
